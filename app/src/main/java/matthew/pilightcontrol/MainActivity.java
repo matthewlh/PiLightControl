9ae2d160183861390b6a2bb69a4b8a7339de1a56
Matthew <matth@ssel.montana.edu>
2016-01-06 20:01:37 -0700

    Added basic server connection stuff to GUI.

--- app/src/main/java/matthew/pilightcontrol/MainActivity.java
+++ app/src/main/java/matthew/pilightcontrol/MainActivity.java
@@ -2,6 +2,9 @@ package matthew.pilightcontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,14 +15,24 @@ public class MainActivity extends AppCompatActivity {
     private TextView brightnessTextView;
     private SeekBar brightnessSeekBar;
 
+    private EditText addressEditText;
+    private EditText portEditText;
+    private Button connectButton;
+    private boolean connected;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //init variables
-        brightnessTextView = (TextView) findViewById(R.id.brightnessTextView);
-        brightnessSeekBar = (SeekBar) findViewById(R.id.brightnessSeekBar);
+        brightnessTextView  = (TextView) findViewById(R.id.brightnessTextView);
+        brightnessSeekBar   = (SeekBar)  findViewById(R.id.brightnessSeekBar);
+        addressEditText     = (EditText) findViewById(R.id.addressEditText);
+        portEditText        = (EditText) findViewById(R.id.portEditText);
+
+        connectButton = (Button) findViewById(R.id.connectButton);
+        connected = false;
 
         updateBrightnessTextView();
 
@@ -37,6 +50,25 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) { }
         });
 
+    }
+
+    // respond to click event on connectButton
+    public void connectButtonOnClick(View view)
+    {
+        // respond to button press
+        connected = !connected;
+
+        // update button text and address fields
+        if(connected) {
+            connectButton.setText(R.string.Disconnect);
+            addressEditText.setEnabled(false);
+            portEditText.setEnabled(false);
+        }
+        else {
+            connectButton.setText(R.string.Connect);
+            addressEditText.setEnabled(true);
+            portEditText.setEnabled(true);
+        }
     }
 
     // update the brightnessTextView based on brightnessSeekBar's value
